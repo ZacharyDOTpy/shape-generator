c7a6a66d0693fe701a913d72d1ad3f891cbbe0fe
Zach <zach.Brewer.1995@gmail.com>
2024-02-01 17:52:39 -0500

    added init function

--- index.js
+++ index.js
@@ -43,3 +43,42 @@ const questions = [
         validate: (value) => !!value.trim() || 'Please choose a color or hex value',
   },
 ]
+
+function init () {
+  prompt(questions)
+  .then(data => {
+    const shapeResponse = shapeResponse(data.shapePrompt);
+    const textResponse = textResponse(data.textPrompt);
+    const shapeColor = data.shapeColorPrompt;
+    const textColor = data.textColorPrompt;
+
+    const generateShape = createShape(shapeResponse);
+
+    function populateShape() {
+      const populatedShape = new generateShape(shapeColor, textResponse, textColor);
+
+      return populatedShape;
+    }
+
+    const populatedShape = populateShape();
+
+    function writeToFile(filename, populatedShape) {
+      const genLogo = generateLogo(populatedShape);
+
+      if (!fs.existsSync('./output')) {
+        fs.mkdirSync('./output')
+      }
+
+      fs.writeFileSync('./output/logo.svg', genLogo, err => {
+        if (err) {
+          console.error(err);
+        }
+      })
+    }
+
+    writeToFile('logo.svg', populatedShape)
+    console.log('Generated logo.svg');
+  })
+};
+
+init();
